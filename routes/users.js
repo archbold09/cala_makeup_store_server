23352488448474ebb1317edcac09e6d6acdd92b0
angel_archbold <angelarchbold09@gmail.com>
2021-01-02 07:03:15 -0500

    Login y conexion con front end

--- routes/users.js
+++ routes/users.js
@@ -17,5 +17,20 @@ function usersApi(app) {
       console.log(`Error: ${error}`);
     }
   });
+
+  router.post("/login", async (req, res) => {
+    console.log(req.body);
+    res.status(200).json({
+       data: req.body 
+    });
+    // try {
+    //   // const users = await UsersService.getUsers();
+    //   // res.status(200).json({
+    //   //   data: users,
+    //   // });
+    // } catch (error) {
+    //   console.log(`Error: ${error}`);
+    // }
+  });
 }
 module.exports = usersApi;
